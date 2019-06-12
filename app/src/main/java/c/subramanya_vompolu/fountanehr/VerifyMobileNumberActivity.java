a488a2bb8781b74fbbdab578d2e09350f390eb98
Subramanya Vompolu <subbuviz@gmail.com>
2019-06-12 13:12:25 +0530

    UI v1

---
+++ app/src/main/java/c/subramanya_vompolu/fountanehr/VerifyMobileNumberActivity.java
@@ -0,0 +1,13 @@
+package c.subramanya_vompolu.fountanehr;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class VerifyMobileNumberActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_verify_mobile_number);
+    }
+}
